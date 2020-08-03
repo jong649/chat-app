5e7e470b37a4805fb1451d2e64ca0651e8b315e2
Jon <jong649@gmail.com>
2020-08-03 15:39:40 -0700

    add message form, send message to all users, notify when users join or leave

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,15 @@
-io();
+const socket = io();
+
+socket.on("message", (message) => {
+  console.log(message);
+});
+
+document.getElementById("form_message").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const messageText = e.target.elements.message.value;
+  socket.emit("sendMessage", messageText);
+});
+
+socket.on("sendMessage", (message) => {
+  console.log("Here is the new message", message);
+});
