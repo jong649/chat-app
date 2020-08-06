814ab91e3aa1e89e6e730590a8cbd61c5467a944
Jon <jong649@gmail.com>
2020-08-06 14:49:10 -0700

    usernames appear with messages now

--- public/js/chat.js
+++ public/js/chat.js
@@ -19,6 +19,7 @@ const { username, room } = Qs.parse(location.search, {
 socket.on("locationMessage", (url) => {
   console.log(url);
   const html = Mustache.render(locationTemplate, {
+    username: url.username,
     url: url.url,
     createdAt: moment(url.createdAt).format("h:mm a"),
   });
@@ -28,6 +29,7 @@ socket.on("locationMessage", (url) => {
 socket.on("message", (message) => {
   console.log(message);
   const html = Mustache.render(messageTemplate, {
+    username: message.username,
     message: message.text,
     createdAt: moment(message.createdAt).format("h:mm a"),
   });
@@ -69,4 +71,9 @@ $sendLocationButton.addEventListener("click", () => {
   });
 });
 
-socket.emit("join", { username, room });
+socket.emit("join", { username, room }, (error) => {
+  if (error) {
+    alert(error);
+    location.href = "/";
+  }
+});
